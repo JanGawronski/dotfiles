8f71f93f4e98cda18824eecb9d348098ffa1f513
Jan Gawroński <jangawronski04@gmail.com>
2026-04-01 16:27:46 +0200

    Removed thumbnail cropping from yt-dlp config

--- module/home/yt-dlp.nix
+++ module/home/yt-dlp.nix
@@ -6,7 +6,6 @@
       audio-quality = 0;
       remux-video = "mp4>mkv";
       sponsorblock-remove = "all";
-      ppa = "\"ThumbnailsConvertor+FFmpeg_o:-c:v mjpeg -vf crop='min(iw\,ih)':min'(iw\,ih)',scale=w=500:h=500:force_original_aspect_ratio=decrease,setsar=1\"";
       embed-thumbnail = true;
       embed-metadata = true;
       output = "%(title)s.%(ext)s";
