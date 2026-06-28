0e88bf8c0f7f55b9ab1280603c7500922a1c85e2
Jan Gawroński <jangawronski04@gmail.com>
2026-06-29 00:38:11 +0200

    Added aliases for audiobook and music

--- module/home/fish.nix
+++ module/home/fish.nix
@@ -9,6 +9,8 @@
       df = "df --human-readable";
       du = "du --human-readable --summarize";
       tree = "tree -L 2";
+      book = "mpv --no-video --save-position-on-quit";
+      music = "mpv --shuffle --loop-playlist --no-video";
     };
     interactiveShellInit = "set -g fish_greeting";
   };
